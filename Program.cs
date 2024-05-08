8e35c971094d0aff2de8a525df2200a526596c88
Danil Skalyanskiy <dskalyanskkiy@gmail.com>
2024-05-08 22:25:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class CustomException : Exception
+{ 
+    public CustomException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            ProcessData(15);
+
+        }
+        catch (CustomException ex)
+        {
+            Console.WriteLine("Власний виняток: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Вийшла помилка: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Програма добре відпрацювала");
+        }
+    }
+
+    static void ProcessData(int value)
+    {
+        if (value > 100)
+        {
+            throw new CustomException("Значення не повинно бути більше ніж 100");
+        }
+        Console.WriteLine("Обробка даних для значення: " + value);
+    }
+}
